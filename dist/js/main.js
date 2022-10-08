8e9c509c8e8e9fab26eb88c2ed42d17859c87f3a
Abdalrhman Alshykh Deeb <abdalrhmanalshykhdeeb@Abdalrhmans-MacBook-Pro.local>
2022-10-09 02:37:37 +0300

    18

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,3 +18,41 @@ toggle.onclick = function () {
 toggleMisc.onclick = () => {
   toggleMisc.classList.toggle("fa-angle-down");
 };
+
+let fullScreen = document.getElementById("toggle-fullscreen");
+
+fullScreen.onclick = () => {
+  fullScreen.classList.toggle("full-screen");
+  if (fullScreen.toString("full-screen")) {
+    openFullscreen();
+  } else {
+    closeFullscreen();
+  }
+};
+
+let elem = document.documentElement;
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
+    document.msExitFullscreen();
+  }
+}
