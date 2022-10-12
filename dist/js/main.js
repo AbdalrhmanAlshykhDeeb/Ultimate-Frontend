e6dee597941bd4a1dac7f7e9a3907d3834a778bb
Abdalrhman Alshykh Deeb <abdalrhmanalshykhdeeb@Abdalrhmans-MacBook-Pro.local>
2022-10-12 20:45:50 +0300

    27

--- dist/js/main.js
+++ dist/js/main.js
@@ -77,3 +77,13 @@ $(".color-options li").on("click", function () {
     .removeClass(themesClasses.join(" "))
     .addClass($(this).data("theme"));
 });
+
+var fontClasses = [];
+$(".font-options select option").each(function () {
+  fontClasses.push($(this).val());
+});
+$(".font-options select").on("change", function () {
+  $("body")
+    .removeClass(fontClasses.join(" "))
+    .addClass($(this).find("option:selected").val());
+});
